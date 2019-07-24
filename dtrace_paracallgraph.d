f56cdc28fdc8d78025ea8b723ef3a4fd65a0182c
gadrenayan <gadrenayan@gmail.com>
2019-07-24 22:15:02 +0530

    Create dtrace_paracallgraph.d

---
+++ dtrace_paracallgraph.d
@@ -0,0 +1,15 @@
+# This will give a callgraph of all user defined functions.
+# usage: ./dtrace_paracallgraph.d -c '/bin/ls'
+# prerequisite: executable should not be stripped of symbols.
+#               dtrace relies on function symbol names to be present.
+
+#!/usr/sbin/dtrace -s
+
+#pragma D option flowindent
+
+pid$target:a.out::entry
+{
+}
+pid$target:a.out::return
+{
+}
